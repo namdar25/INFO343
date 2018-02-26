deaab3bace096c9ab6ee4f6da6a32e26297f34a2
namdar25 <namdar25@gmail.com>
2018-02-26 14:10:40 -0800

    firebase initialized

--- subuw/src/index.js
+++ subuw/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase'
+  
+  let config = {
+    apiKey: "AIzaSyCg0NK0JvgjlU8pHF_kNxLnx_To1NhogNg",
+    authDomain: "subuw-j420m.firebaseapp.com",
+    databaseURL: "https://subuw-j420m.firebaseio.com",
+    projectId: "subuw-j420m",
+    storageBucket: "subuw-j420m.appspot.com",
+    messagingSenderId: "525275022235"
+  };
+  firebase.initializeApp(config);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
