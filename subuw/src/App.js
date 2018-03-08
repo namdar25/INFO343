d0b9ec22c64acd301090352a5a3c94eeda1903ae
jwcantle <jwcantle@uw.edu>
2018-03-08 13:17:02 -0800

    start of chat/convo

--- subuw/src/App.js
+++ subuw/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import
+import Chat from './Chat.js';
 
-  class App extends Component {
+class App extends Component {
 
-    render() {
-      return (
-        <div className="App">
-
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div className="App">
+        <Chat />
+      </div>
+    )
   }
+}
 
 export default App;
--- subuw/src/index.js
+++ subuw/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase'
-  
-  let config = {
-    apiKey: "AIzaSyCg0NK0JvgjlU8pHF_kNxLnx_To1NhogNg",
-    authDomain: "subuw-j420m.firebaseapp.com",
-    databaseURL: "https://subuw-j420m.firebaseio.com",
-    projectId: "subuw-j420m",
-    storageBucket: "subuw-j420m.appspot.com",
-    messagingSenderId: "525275022235"
-  };
-  firebase.initializeApp(config);
+
+let config = {
+  apiKey: "AIzaSyCg0NK0JvgjlU8pHF_kNxLnx_To1NhogNg",
+  authDomain: "subuw-j420m.firebaseapp.com",
+  databaseURL: "https://subuw-j420m.firebaseio.com",
+  projectId: "subuw-j420m",
+  storageBucket: "subuw-j420m.appspot.com",
+  messagingSenderId: "525275022235"
+};
+firebase.initializeApp(config);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
