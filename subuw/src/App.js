b90fd427f4e56fb7b7baf29d661c8de29a780126
Raffi Gharakhanian <raffig@uw.edu>
2018-03-10 10:55:24 -0800

    No changes

--- subuw/src/App.js
+++ subuw/src/App.js
@@ -12,11 +12,6 @@ import {
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Main from './Main';
-import '../node_modules/jquery/dist/jquery.js';
-import '../node_modules/popper.js/dist/popper.js';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import { StartPage } from './StartPage';
 import { About } from './About';
 import firebase from 'firebase';
