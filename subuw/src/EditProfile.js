2f8d2db600b00d5570db4e55e5162ad1f3016ca5
namdar25@gmail.com <namdar25@gmail.com>
2018-03-12 16:05:06 -0700

    pre merge store

--- subuw/src/EditProfile.js
+++ subuw/src/EditProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 //import {ImageUploader} from './node_modules/react-image-upload';
-import {firebase} from 'firebase';
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
 import {
     Button, Modal, ModalHeader, ModalBody, ModalFooter, Collapse,
     Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink
@@ -23,13 +25,41 @@ export class EditProfile extends Component {
     }
 
 	componentDidMount() {
-	this.setState({
+		this.setState({
 		displayName : this.props.user.displayName,
 		photo: this.props.user.photo,
         email: this.props.user.email,
         phoneNumber: this.props.user.phoneNumber,
 		userID : this.props.user.userID
 		})
+		
+		this.dataRef = firebase.database().ref('imgs/');
+		this.storageRef = firebase.storage().ref('imgs/');
+	}
+
+	fileChange(event) {
+		let profileRef = firebase.database().ref('Users/' + this.state.userID);
+		let name = event.target.files[0].name;
+		let file = event.target.files[0];
+		console.log(name, file)
+		let imgRef = this.storageRef.child(name);
+		imgRef.put(file).then((snapshot) => {
+			let url = snapshot.downloadURL;
+			this.setState({
+				img: url
+			})
+			this.profileRef.set({
+				profilePicture : url
+			})
+			this.dataRef.push({
+				imageurl: url
+			}).catch(err => {
+				this.setState({
+					errorMessage: err.message
+				})
+			})
+		});
+
 	}
 
     handleChange(propertyName, event) {
@@ -38,21 +68,20 @@ export class EditProfile extends Component {
         this.setState(change)
     }
 
-    editProfile(event) {
+	editProfile(event) {
+		let profileRef = firebase.database().ref('Users/' + this.state.userID);
         event.preventDefault();
 		let displayName = this.state.displayName;
-		let photo = this.state.photo;
         let email = this.state.city;
         let phoneNumber = this.state.phoneNumber;
 		let userID = this.state.userID
 
-        let profileRef = firebase.database().ref('Users/ + userID');
         profileRef.set({
             displayName : displayName,
-			photo: photo,
             email: email,
             phoneNumber: phoneNumber,
-			userID : userID
+			userID: userID,
+			
 		}).catch(err => {
 			this.setState({ errorMessage: err.message })
 		});
@@ -69,7 +98,7 @@ export class EditProfile extends Component {
     render() {
         return (
             <div>
-                <div className="card card-inverse" onClick={this.toggle}>
+				<div className="card card-inverse" id="profileCard" onClick={this.toggle}>
                     <img className="card-img" src={this.props.user.image} alt="Profile Picture" />
                     <div className="card-img-overlay" id='profile-card'>
                         <h4 className="card-title">{this.props.user.displayName}</h4>
@@ -92,22 +121,17 @@ export class EditProfile extends Component {
                             <div className='addListing' >
 								<form>
 									<div className="column">
-										< div className="form-group">
+										<div className="form-group">
 											<img className="card-img" src={this.props.user.image} alt="Profile Picture" />
 											<label>Profile Picture </label>
-											{/*<ImageUploader
-												withIcon={true}
-												buttonText='Choose Profile Picture'
-												onChange={this.handleChange.bind(this, 'photo')}
-												imgExtension={['.jpg', '.gif', '.png', '.gif']}
-												maxFileSize={5242880}
-											/>*/}
+											<label>Upload New Profile Picture</label>
+											<input type="file" onChange={(e) => this.fileChange(e)} multiple />
 										</div>
-										< div className="form-group">
+										<div className="form-group">
 											<label>email</label>
 											<input type="text" className="form-control" id="email" placeholder={this.props.user.email} onChange={this.handleChange.bind(this, 'email')} />
 										</div>
-										< div className="form-group">
+										<div className="form-group">
 											<label>Phone Number</label>
 											<input type="text" className="form-control" id="phoneNumber" placeholder={this.props.user.phoneNumber} onChange={this.handleChange.bind(this, 'phoneNumber')} />
 										</div>
--- subuw/src/Profile.js
+++ subuw/src/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase'
 import {EditProfile} from './EditProfile';
-import {AddListing} from './AddListing';
+import {EditListing} from './EditListing';
 import {Listing} from './Listing';
 
 export class Profile extends Component {
@@ -24,23 +24,24 @@ constructor(props) {
 				let user = snapshot.val();
 				console.log(user);
 				this.setState({ user: user });
+				this.setState({ authUser: authUser });
 			})
 
 			
 			this.uLRef = firebase.database().ref('Listings/');
-			this.uLRef.on('value', (snapshot) => {
-				
+			this.uLRef.on('value', (snapshot) => {	
 				let userListings = snapshot.val();
 				userListings = Object.values(userListings);
-				console.log(userListings);
-				userListings.filter(listing => listing.uid === authUser.uid)
-				this.setState({ userListings: userListings})
+				userListings = userListings.filter(listing => listing.uid === authUser.uid);
+				this.setState({ userListings: userListings })
+				
 			})
-			this.lLRef = firebase.database().ref('Users/' + authUser.uid +'/LikedListings/');
+			
+			/*this.lLRef = firebase.database().ref('Users/' + authUser.uid +'/LikedListings/');
 			this.lLRef.on('value', (snapshot) => {
 				let likedListings = snapshot.val();
 				this.setState({likedListings:likedListings})
-			})
+			})*/
 		}
 	}
 
@@ -54,39 +55,42 @@ constructor(props) {
 	render() {
 
 		if (this.state.user != null) {
+			let listings = this.state.userListings;
 			let userListingsList = null;
 			if (this.state.userListings != null) {
 				console.log(this.state.userListings);
 				userListingsList = this.state.userListings === null ? [] : Object.keys
 					(this.state.userListings).map((x) => {
-						let userListing = userListingsList[x];
+						console.log(userListingsList);
+						let userListing = listings[x];
 						userListing.key = x;
 						return userListing;
 					});
 			}
-			let likedListingsList = null;
+			/*let likedListingsList = null;
 			if (this.state.likedListings != null) {
+				console.log(this.state.likedListings);
 				likedListingsList = this.state.likedListings === null ? [] : Object.keys
 					(this.state.likedListings).map((x) => {
 						let likedListing = likedListingsList[x];
 						likedListing.key = x;
 						return likedListing;
 					});
-			}
+			}*/
 			return (
 				<div>
 					<EditProfile user={this.state.user}/>
 					<div>
 						{this.state.userListings != null &&
 							userListingsList.map((d, i) => {
-								return <AddListing key={i} listings={d} />
+							return <EditListing key={i} listing={d} uid={this.state.authUser.uid}/>
 							})
 						}
 						{this.state.userListings == null &&
 								<p> Post A Sublease! </p>
 						}
 					</div>		
-					<div>
+					{/*<div>
 						{this.state.likedListings &&
 							likedListingsList.map((d, i) => {
 								return <AddListing key={i} listings={d} />
@@ -95,7 +99,7 @@ constructor(props) {
 						{!this.state.likedListings &&
 								<p> Post A Sublease! </p>
 						}
-					</div>
+					</div>*/}
 				</div>
 			)
 		} else {
